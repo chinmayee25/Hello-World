3e99c3ce1c70e5dd43007b7a1f77577bf29e3474
Chinmayee <chinmayee.thakur11@gmail.com>
2021-08-06 20:06:20 -0400

    first commit

---
+++ simple_buy_sell.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  6 19:59:51 2021
+
+@author: chinm
+"""
+
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import numpy as np
+abb = np.random.randint(1, 100,200)
+
+print(abb)
